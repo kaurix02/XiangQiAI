c6e0bda9c440735afdec309271fa314885ec4928
Kaur <kaurix02@hotmail.com>
2017-12-19 12:03:08 +0200

    Moves asking support

--- board.py
+++ board.py
@@ -10,29 +10,31 @@ S S S S S
          
 RHEAGAEHR
 """
+import os
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
 
 class Board:
 	def __init__(self, strboard = None):	#strboard for if you want to load a board from string
+		self.player = 0
 		if strboard == None:
 			self.initboard()
 		else:
 			self.initboard(strboard)
-		print(self.board)
+		self.show()
 	def initboard(self, strboard = defboard):
 		self.board = []	#all pieces on board
 		for i in range(10):
 			self.board.append([])
 			for j in range(9):
-				self.board[i].append([])
+				self.board[i].append(None)
 		i = 0	#Row number
 		j = 0	#column number
-		pl = 0
+		pl = 0	#pl0 is Player 1, pl1 is Player 2
 		for piece in defboard:
 			if piece == '\n':
 				continue
-			print("*"+piece+"*")
 			if piece!=' ' and piece!='\n':
-				print(piece, i, j)
 				self.board[i][j]=Piece(piece,i,j, pl)
 
 			j += 1
@@ -41,6 +43,22 @@ class Board:
 				i += 1
 				if i>5:
 					pl = 1
+	def show(self):
+		clear()
+		for row in self.board:
+			print("|", end="")
+			for piece in row:
+				if piece==None:
+					print("  ", end="|")
+				else:
+					print(piece, end="|")
+			print()
+	def canmove(self):
+		pass
+	def getmoves(self):
+		pass
+	def makemove(self, x1, y1, x2, y2):
+		pass
 class Piece:
 	def __init__(self, name,x,y,pl):
 		self.name = name
@@ -48,8 +66,108 @@ class Piece:
 		self.y = y
 		self.pl = pl
 	def __str__(self):
-		return self.name
+		return self.name+str(self.pl)
 	def __repr__(self):
-		return self.name
+		return self.name+str(self.pl)
+	def getmoves(self):
+		if self.name=='S':	#Soldier/pawn
+			if self.x<=5 and self.pl==0:	#On player's side
+				return [(self.x+1, self.y)]
+			elif self.x>5 and self.pl==1:	#On player's side
+				return [(self.x-1, self.y)]
+			else:	#Across the river
+				moves=[]
+				if self.pl==0:	#Across the river
+					if self.x<9:
+						moves.append((self.x+1,self.y))
+				else:
+					if self.x>0:
+						moves.append((self.x-1,self.y))
+				if self.y>0:
+					moves.append((self.x,self.y-1))
+				if self.y<8:
+					moves.append((self.x,self.y+1))
+				return moves
+		elif self.name=='R' or self.name=='C':	#Rook/Chariot, Cannon
+			moves=[]
+			for i in range(0,self.x):
+				moves.append((i,self.y))
+			for i in range(self.x+1,10):
+				moves.append((i,self.y))
+			for i in range(0,self.y):
+				moves.append((self.x,i))
+			for i in range(self.y+1,9):
+				moves.append((self.x,i))
+			return moves
+		elif self.name=='H':	#Horse/Knight
+			boxy=list(range(9))
+			boxx=list(range(10))
+			moves=[]
+			if self.x+2 in boxx:
+				if self.y+1 in boxy:
+					moves.append((self.x+2,self.y+1))
+				if self.y-1 in boxy:
+					moves.append((self.x+2,self.y-1))
+			if self.x+1 in boxx:
+				if self.y+2 in boxy:
+					moves.append((self.x+1,self.y+2))
+				if self.y-2 in boxy:
+					moves.append((self.x+1,self.y-2))
+			if self.x-1 in boxx:
+				if self.y+2 in boxy:
+					moves.append((self.x-1,self.y+2))
+				if self.y-2 in boxy:
+					moves.append((self.x-1,self.y-2))
+			if self.x-2 in boxx:
+				if self.y+1 in boxy:
+					moves.append((self.x-2,self.y+1))
+				if self.y-1 in boxy:
+					moves.append((self.x-2,self.y-1))
+			return moves
+		elif self.name=='E':	#Elephant/Bishop
+			moves=[]
+			if self.x==0 or self.x==4:
+				moves.append((2,self.y-2))
+				moves.append((2,self.y+2))
+			elif self.x==5 or self.x==9:
+				moves.append((8,self.y-2))
+				moves.append((8,self.y+2))
+			elif self.y==0:
+				moves.append((self.x-2,2))
+				moves.append((self.x+2,2))
+			elif self.y==8:
+				moves.append((self.x-2,6))
+				moves.append((self.x+2,6))
+			else:	#Center column
+				moves.append((self.x-2,self.y-2))
+				moves.append((self.x+2,self.y-2))
+				moves.append((self.x-2,self.y+2))
+				moves.append((self.x+2,self.y+2))
+			return moves
+		elif self.name=='A':	#Advisor
+			if self.x==0 or self.x==2:
+				return [(1,4)]
+			elif self.x==9 or self.x==7:
+				return [(8,4)]
+			else:
+				moves=[]
+				moves.append((self.x+1,3))
+				moves.append((self.x+1,5))
+				moves.append((self.x-1,3))
+				moves.append((self.x-1,5))
+				return moves
+		elif self.name=='G':	#Governator
+			boxy=list(range(3,6))
+			boxx=list(range(3))+list(range(7,10))
+			moves=[]
+			if self.x+1 in boxx:
+				moves.append((self.x+1,self.y))
+			if self.x-1 in boxx:
+				moves.append((self.x-1,self.y))
+			if self.y+1 in boxy:
+				moves.append((self.x,self.y+1))
+			if self.y-1 in boxy:
+				moves.append((self.x,self.y-1))
+			return moves
 
 Board()
