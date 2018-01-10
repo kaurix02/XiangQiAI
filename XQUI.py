34b577cc988576339889acbc254d83cc6677a6a7
Kaur <kaurix02@hotmail.com>
2018-01-11 01:46:03 +0200

    GUI looks good, fails to update

--- XQUI.py
+++ XQUI.py
@@ -8,12 +8,13 @@ class XiangQi():
 		self.board = Board()
 		self.pl2 = players[pl2](1, self.board)  # Turn class into object for player2
 		self.background_image = tk.PhotoImage(file="images/board.gif")	#Use board as bgd
-		self.background_label = tk.Label(self.master, image=self.background_image, width=580, height=640)
+		# self.background_label = tk.Label(self.master, image=self.background_image, width=580, height=640)
 		self.canvas = tk.Canvas(self.master, width=580, height=640)
 		self.canvas.pack()
-		self.background_label.place(x=0, y=0, width=580, height=640)
+		# self.background_label.place(x=0, y=0, width=580, height=640)
 		self.getImages()	#Dict of images
 		self.moves = 0
+		self.mv_fr = None
 		self.play()
 	def getImages(self):	#Helper method that adds an image reference to each piece
 		self.im = {}
@@ -41,25 +42,32 @@ class XiangQi():
 			else:
 				print("Game over, winner: " + str(self.board.won))
 	def draw(self):
-		self.buttons = []
+		self.canvas.delete("all")
+		self.canvas.create_image(0,0,image=self.background_image,anchor='nw')
 		for i in range(10):	#Row
 			for j in range(9):	#Column
 				if self.board.board[i][j] is None:	#Empty
 					t = self.canvas.create_rectangle((20+j*60,560-i*60,80+j*60,620-i*60), fill="red")
-					self.canvas.tag_bind(t,"<Button-1>",lambda x:print(x.x,x.y)) #self.moveTo(i,j))
-					self.buttons.append(t)
+					self.canvas.tag_bind(t,"<Button-1>",lambda x:self.click(x)) #self.moveTo(i,j))
 					#tk.Button(self.master, command=lambda: self.moveTo(i,j), width=5, height=5, image=None, bd=0).grid(row=9-i,column=j)
 				elif self.board.board[i][j].pl == self.board.player:	#Friendly
 					#tk.Button(self.master, command=lambda: self.moveFrom(self.board.board[i][j]), image=self.im[str(self.board.board[i][j])], bd=0).grid(row=9-i,column=j)
 					t = self.canvas.create_image(50+j*60,590-i*60,image=self.im[str(self.board.board[i][j])])
-					self.canvas.tag_bind(t,"<Button-1>",lambda x:self.moveFrom(self.board.board[i][j]))
-					self.buttons.append(t)
+					self.canvas.tag_bind(t,"<Button-1>",lambda x:self.click(x))
 				else:	#Hostile
 					#tk.Button(self.master, command=lambda: self.moveTo(i,j), image=self.im[str(self.board.board[i][j])], bd=0).grid(row=9-i,column=j)
 					t = self.canvas.create_image(50+j*60,590-i*60,image=self.im[str(self.board.board[i][j])])
-					self.canvas.tag_bind(t,"<Button-1>",lambda x:self.moveTo(i,j))
-					self.buttons.append(t)
-		self.m_fr = None
+					self.canvas.tag_bind(t,"<Button-1>",lambda x:self.click(x))
+	def click(self, event):
+		b,a= int((event.x-20)/60), int((event.y-20)/60)
+		a = 9-a
+		print(a,b)
+		if self.board.board[a][b] is None:
+			self.moveTo(a,b)
+		elif self.board.board[a][b].pl == self.board.player:
+			self.moveFrom(self.board.board[a][b])
+		else:
+			self.moveTo(a,b)
 	def moveFrom(self, piece):
 		print("Moving with "+str(piece))
 		self.m_fr = piece
@@ -73,7 +81,7 @@ class XiangQi():
 				self.board.make_move(self.m_fr,x,y)
 				self.play()
 			else:
-				print(x,y)
+				print("Bad move: ",x,y)
 		print(self.m_fr)
 r=tk.Tk()
 app = XiangQi(1,r)
