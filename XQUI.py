4c29c0b9fbe437c6e34842a5009c4e6019f2d721
Kaur <kaurix02@hotmail.com>
2018-01-10 23:36:53 +0200

    GUI almost showing

--- XQUI.py
+++ XQUI.py
@@ -2,13 +2,13 @@ import tkinter as tk
 from play import players
 from board import Board
 
-class XiangQi(tk.Frame):
+class XiangQi():
 	def __init__(self, pl2, master=None):
-		tk.Frame.__init__(self,master)
+		self.master = master
 		self.board = Board()
 		self.pl2 = players[pl2](1, self.board)  # Turn class into object for player2
-		self.background_image=tk.PhotoImage("images/board.gif")
-		self.background_label = tk.Label(self, image=self.background_image)
+		self.background_image = tk.PhotoImage("images/board.gif")	#Use board as bgd
+		self.background_label = tk.Label(self.master, image=self.background_image)
 		self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 		self.getImages()	#Dict of images
 		self.moves = 0
@@ -24,6 +24,7 @@ class XiangQi(tk.Frame):
 			if self.board.player == 0:
 				self.moves += 1
 				print("Player turn")
+				self.avail_moves,_ = self.board.get_moves()
 				self.draw()
 			else:
 				pl2.move()		
@@ -41,21 +42,27 @@ class XiangQi(tk.Frame):
 		for i in range(10):	#Row
 			for j in range(9):	#Column
 				if self.board.board[i][j] is None:	#Empty
-					tk.Button(self, command=lambda: self.moveTo(i,j), width=60, height=60).grid(row=i,column=j)
+					tk.Button(self.master, command=lambda: self.moveTo(i,j), width=5, height=5, image=None).grid(row=9-i,column=j)
 				elif self.board.board[i][j].pl == self.board.player:	#Friendly
-					tk.Button(self, command=lambda: self.moveFrom(self.board.board[i][j]), image=self.im[str(self.board.board[i][j])]).grid(row=i,column=j)
+					tk.Button(self.master, command=lambda: self.moveFrom(self.board.board[i][j]), image=self.im[str(self.board.board[i][j])]).grid(row=9-i,column=j)
 				else:	#Hostile
-					tk.Button(self, command=lambda: self.moveTo(i,j), image=self.im[str(self.board.board[i][j])]).grid(row=i,column=j)
+					tk.Button(self.master, command=lambda: self.moveTo(i,j), image=self.im[str(self.board.board[i][j])]).grid(row=9-i,column=j)
 		self.m_fr = None
 	def moveFrom(self, piece):
+		print("Moving with "+str(piece)
 		self.m_fr = piece
 	def moveTo(self, x, y):
 		if self.m_fr is None:	#No piece selected, do nothing.
 			return
 		if self.m_fr in self.avail_moves:
-			if (x,y) in self.avail_moves(self.m_fr):
+			if (x,y) in self.avail_moves[self.m_fr]:
+				print("YAY")
 				self.board.make_move(self.m_fr,x,y)
 				self.play()
-app = XiangQi(1)
-app.master.title("XiangQi")
-app.mainloop()
+			else:
+				print(x,y)
+		print(self.m_fr)
+r=tk.Tk()
+app = XiangQi(1,r)
+#r.master.title("XiangQi")
+r.mainloop()
