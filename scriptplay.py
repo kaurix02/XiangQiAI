a1e30e60d25054e9b9e089de374636dacf0e4409
MariLiis <mariliisvelner@gmail.com>
2018-01-10 02:30:03 +0200

    Fixed typo

--- scriptplay.py
+++ scriptplay.py
@@ -2,5 +2,4 @@ import play
 import sys
 
 pls = play.players
-print(play.run_test_sm(pls[int(sys.argv[1])], pls[int(sys.argv[2])],10)9
-
+print(play.run_test_sm(pls[int(sys.argv[1])], pls[int(sys.argv[2])], 10))
